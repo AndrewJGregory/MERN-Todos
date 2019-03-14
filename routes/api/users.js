0d7b874b9a9155d340c2faa61a381dc28aa566e8
Andrew Gregory <andrewjgregoryajg@gmail.com>
2019-03-14 15:53:15 -0400

    send signed token when signing in/up

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const User = require("../../models/User");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
 
 router.post("/signup", (req, res) => {
   const { username, password } = req.body;
@@ -21,7 +23,17 @@ router.post("/signup", (req, res) => {
           newUser.password = hash;
           newUser
             .save()
-            .then(user => res.json(user))
+            .then(user => {
+              const payload = { name: user.name, id: user.id };
+              jwt.sign(
+                payload,
+                keys.secretOrKey,
+                { expiresIn: 3600 },
+                (err, token) => {
+                  res.json({ success: true, token: `Bearer: ${token}` });
+                },
+              );
+            })
             .catch(err => console.log(err));
         });
       });
@@ -39,7 +51,15 @@ router.post("/signin", (req, res) => {
     }
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
-        res.json({ msg: "success!" });
+        const payload = { name: user.name, id: user.id };
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({ success: true, token: `Bearer ${token}` });
+          },
+        );
       } else {
         return res.status(400).json({ password: "incorrect password" });
       }
