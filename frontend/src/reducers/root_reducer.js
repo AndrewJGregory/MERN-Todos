da984be5a266ccf6e951bb01af4605863e77bc53
Andrew Gregory <andrewjgregoryajg@gmail.com>
2019-04-12 14:45:45 -0400

    add users reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,8 +1,9 @@
 import { combineReducers } from "redux";
 import errors from "./errors_reducer";
 import session from "./session_reducer";
-import todos from "./todos_reducer.js";
+import todos from "./todos_reducer";
+import users from "./users_reducer";
 
-const rootReducer = combineReducers({ session, errors, todos });
+const rootReducer = combineReducers({ session, errors, todos, users });
 
 export default rootReducer;
