1ffa0b37a94d4ca4086c3b36be22c515763a7497
Andrew Gregory <andrewjgregoryajg@gmail.com>
2019-04-16 15:34:28 -0400

    merge todos into store after receiving all todos

--- frontend/src/reducers/todos_reducer.js
+++ frontend/src/reducers/todos_reducer.js
@@ -1,5 +1,10 @@
+import { RECEIVE_TODOS } from "../actions/todo_actions";
+
 export default function todosReducer(state = {}, action) {
+  Object.freeze(state);
   switch (action.type) {
+    case RECEIVE_TODOS:
+      return Object.assign({}, state, action.payload.todos);
     default:
       return state;
   }
