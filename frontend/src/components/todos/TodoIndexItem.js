edf243291f0af241d447f3c4f4e9fb910fc131a1
Andrew Gregory <andrewjgregoryajg@gmail.com>
2019-05-03 18:51:13 -0400

    space out todos

--- frontend/src/components/todos/TodoIndexItem.js
+++ frontend/src/components/todos/TodoIndexItem.js
@@ -1,10 +1,12 @@
+import "./todo_index_item.css";
+
 import { Button } from "reactstrap";
 import PropTypes from "prop-types";
 import React from "react";
 
 export default function TodoIndexItem({ todo, user }) {
   return (
-    <li>
+    <li className="todo">
       {user.username} has to {todo.content}{" "}
       <Button todoid={todo._id}>Edit</Button>
     </li>
