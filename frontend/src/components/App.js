c27234a6e9aad674705bb27d898d8759f4dd746d
Andrew Gregory <andrewjgregoryajg@gmail.com>
2019-05-08 21:17:53 -0400

    show edit todo modal container on any page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,19 +1,31 @@
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 
-import Main from "./Main";
+import EditTodoModalContainer from "./todos/EditTodoModalContainer";
+import HeaderContainer from "./nav/HeaderContainer";
 import React from "react";
 import SessionFormContainer from "./session/SessionFormContainer";
 import { Switch } from "react-router-dom";
+import TodoIndexContainer from "./todos/TodoIndexContainer";
 import UserShowContainer from "./users/UserShowContainer";
+import { connect } from "react-redux";
+import { currentUserSelector } from "../util/selectors";
 
-export default function App() {
+function App({ isLoggedIn }) {
   return (
-    <Switch>
-      <ProtectedRoute path="/users/:id" component={UserShowContainer} />
-      <ProtectedRoute path="/todos" component={Main} />
-      <AuthRoute path="/signin" component={SessionFormContainer} />
-      <AuthRoute path="/signup" component={SessionFormContainer} />
-      <AuthRoute path="/" component={SessionFormContainer} />
-    </Switch>
+    <>
+      {isLoggedIn && <HeaderContainer />}
+      <EditTodoModalContainer />
+      <Switch>
+        <ProtectedRoute path="/users/:id" component={UserShowContainer} />
+        <ProtectedRoute path="/todos" component={TodoIndexContainer} />
+        <AuthRoute path="/signin" component={SessionFormContainer} />
+        <AuthRoute path="/signup" component={SessionFormContainer} />
+        <AuthRoute path="/" component={SessionFormContainer} />
+      </Switch>
+    </>
   );
 }
+
+const msp = state => ({ isLoggedIn: Boolean(currentUserSelector(state)) });
+
+export default connect(msp)(App);
