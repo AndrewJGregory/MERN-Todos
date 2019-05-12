24504ac2d1a6b332f0fe754382ff52948a163bbc
Andrew Gregory <andrewjgregoryajg@gmail.com>
2019-05-12 19:20:22 -0400

    send email of err report only in production

--- routes/api/emails.js
+++ routes/api/emails.js
@@ -5,31 +5,33 @@ const { emailAddress, emailPassword } = require("../../config/keys");
 router.post("/", createEmail);
 
 async function createEmail(req, res) {
-  let { stack, message, name } = req.body.errorInfo;
-  // how do I persist this transporter across requests?
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    service: "gmail",
-    auth: {
-      user: emailAddress,
-      pass: emailPassword,
-    },
-  });
-  const beginning = `<h1>Message: ${message}</h1><strong>STACK TRACE</strong><br/>`;
-  const html = beginning.concat(
-    stack
-      .split("\n")
-      .map(line => `<li>${line}</li>`)
-      .join(""),
-  );
+  if (process.env.NODE_ENV === "production") {
+    const { stack, message, name } = req.body.errorInfo;
+    // how do I persist this transporter across requests?
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      service: "gmail",
+      auth: {
+        user: emailAddress,
+        pass: emailPassword,
+      },
+    });
+    const beginning = `<h1>Message: ${message}</h1><strong>STACK TRACE</strong><br/>`;
+    const html = beginning.concat(
+      stack
+        .split("\n")
+        .map(line => `<li>${line}</li>`)
+        .join(""),
+    );
 
-  const mailOptions = {
-    from: emailAddress,
-    to: emailAddress,
-    subject: `Error Report: ${name}`,
-    html,
-  };
-  await transporter.sendMail(mailOptions);
+    const mailOptions = {
+      from: emailAddress,
+      to: emailAddress,
+      subject: `Error Report: ${name}`,
+      html,
+    };
+    await transporter.sendMail(mailOptions);
+  }
 }
 
 module.exports = router;
