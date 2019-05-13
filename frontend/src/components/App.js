6e82c27defda536a863290aa61c52e4ea7076c8c
Andrew Gregory <andrewjgregoryajg@gmail.com>
2019-05-13 08:03:39 -0400

    use strict mode for all components

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,9 @@
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
+import React, { StrictMode } from "react";
 
 import EditTodoModalContainer from "./todos/EditTodoModalContainer";
 import ErrorBoundary from "./ErrorBoundary";
 import HeaderContainer from "./nav/HeaderContainer";
-import React from "react";
 import SessionFormContainer from "./session/SessionFormContainer";
 import { Switch } from "react-router-dom";
 import TodoIndexContainer from "./todos/TodoIndexContainer";
@@ -13,22 +13,24 @@ import { currentUserSelector } from "../util/selectors";
 
 function App({ isLoggedIn }) {
   return (
-    <ErrorBoundary>
-      <>
-        {isLoggedIn && <HeaderContainer />}
-        <EditTodoModalContainer />
-        <Switch>
-          <ProtectedRoute
-            path="/users/:username"
-            component={UserShowContainer}
-          />
-          <ProtectedRoute path="/todos" component={TodoIndexContainer} />
-          <AuthRoute path="/signin" component={SessionFormContainer} />
-          <AuthRoute path="/signup" component={SessionFormContainer} />
-          <AuthRoute path="/" component={SessionFormContainer} />
-        </Switch>
-      </>
-    </ErrorBoundary>
+    <StrictMode>
+      <ErrorBoundary>
+        <>
+          {isLoggedIn && <HeaderContainer />}
+          <EditTodoModalContainer />
+          <Switch>
+            <ProtectedRoute
+              path="/users/:username"
+              component={UserShowContainer}
+            />
+            <ProtectedRoute path="/todos" component={TodoIndexContainer} />
+            <AuthRoute path="/signin" component={SessionFormContainer} />
+            <AuthRoute path="/signup" component={SessionFormContainer} />
+            <AuthRoute path="/" component={SessionFormContainer} />
+          </Switch>
+        </>
+      </ErrorBoundary>
+    </StrictMode>
   );
 }
 
